f05af9bf0179f8575b9a18df4364e8ea41d8c4b4
gregorypatlasov1223 <gregory.patlasov@gmail.com>
2025-09-30 22:47:34 +0300

    Серега help please

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define STACK_MAX_CAPACITY 333
 #define STACK_MAX_SIZE  100
@@ -8,18 +9,18 @@
 #define INITIAL_SIZE 0
 #define MULTIPLIER 2
 
-#define CHECK_STACK code_error = stack_verify(stack, __FILE__, __func__, __LINE__); \
+/*#define CHECK_STACK code_error = stack_verify(stack, __FILE__, __func__, __LINE__); \
                     if (code_error) \
                     { \
                         return code_error; \
-                    } \
-
+                    } \         */
 
 enum stack_err_t
 {
     INITIALIZE_ERROR    = 0,
     NO_ERROR            = 1,
     UNDER_FLOW          = 3,
+    OVER_FLOW           = 4,
     ARRAY_POINTER_ERROR = 13,
     SIZE_ERROR          = 52,
     CAPACITY_ERROR      = 666
@@ -38,11 +39,10 @@ struct stack_t
 stack_err_t stack_pop(stack_t *stack, type_of_element *value);
 stack_err_t stack_push(stack_t *stack, type_of_element value);
 stack_err_t stack_creator(stack_t *stack, size_t capacity);
-
-int stack_verify(stack_t *stack, const char *file_name, const char *function_name, int line);
+stack_err_t stack_verify(stack_t *stack, const char *file_name, const char *function_name, int line);
 
 void stack_destructor(stack_t *stack);
-void stack_dump(stack_t *stack, int sum_of_errors, const char *file_name, const char *function_name, int line);
+void stack_dump(stack_t *stack, stack_err_t type_of_error, const char *file_name, const char *function_name, int line);
 
 
 int main(void)
@@ -55,23 +55,59 @@ int main(void)
         return INITIALIZE_ERROR;
     }
 
-    stack_push(&my_stack, 10);
-    stack_push(&my_stack, 20);
-    stack_push(&my_stack, 30);
+    my_stack.size = 1000;  // OVER_FLOW error
+    //my_stack.size = -1;    // UNDER_FLOW error
 
-    type_of_element value;
 
-    if (stack_pop(&my_stack,&value) == NO_ERROR)
+    if (stack_push(&my_stack, 10) != NO_ERROR)
+    {
+        printf("Push failed\n");
+    }
+    if (stack_push(&my_stack, 20) != NO_ERROR)
+    {
+        printf("Push failed\n");
+    }
+    if (stack_push(&my_stack, 30) != NO_ERROR)
+    {
+        printf("Push failed\n");
+    }
+
+    type_of_element value = 0;
+
+    if (stack_pop(&my_stack, &value) == NO_ERROR)
         printf("Popped value = %d\n", value);
-    if (stack_pop(&my_stack,&value) == NO_ERROR)
+    if (stack_pop(&my_stack, &value) == NO_ERROR)
         printf("Popped value = %d\n", value);
-    if (stack_pop(&my_stack,&value) == NO_ERROR)
+    if (stack_pop(&my_stack, &value) == NO_ERROR)
         printf("Popped value = %d\n", value);
+
+    if (stack_pop(&my_stack, &value) != NO_ERROR)
+        printf("Stack pop failed with error (this should show stack_dump)\n");
 
     stack_destructor(&my_stack);
 
     return 0;
 }
+
+
+/*stack_err_t stack_pop(stack_t *stack, type_of_element *value)
+{
+    if (stack == NULL || value == NULL)
+    {
+        return ARRAY_POINTER_ERROR;
+    }
+
+    if ((stack -> size) < 0)
+    {
+        fprintf(stderr, "Error: Stack is underflowed\n");
+        return stack_verify(stack, __FILE__, __func__, __LINE__);
+    }
+
+    *value = stack -> array[(stack -> size) - 1];
+    stack -> size = (stack -> size) - 1;
+
+    return NO_ERROR;
+}*/
 
 
 stack_err_t stack_pop(stack_t *stack, type_of_element *value)
@@ -80,14 +116,19 @@ stack_err_t stack_pop(stack_t *stack, type_of_element *value)
         return ARRAY_POINTER_ERROR;
     }
 
-    if (stack->size <= 0)
+    stack_err_t code_error = stack_verify(stack, __FILE__, __func__, __LINE__);
+    if (code_error != NO_ERROR) {
+        return code_error;
+    }
+
+    if ((stack -> size) <= 0)
     {
         fprintf(stderr, "Error: Stack is underflowed\n");
         return UNDER_FLOW;
     }
 
-    *value = stack->array[stack->size];
-    stack->size = stack->size - 1;
+    *value = stack -> array[(stack -> size) - 1];
+    stack -> size = (stack -> size) - 1;
 
     return NO_ERROR;
 }
@@ -95,7 +136,12 @@ stack_err_t stack_pop(stack_t *stack, type_of_element *value)
 
 stack_err_t stack_push(stack_t *stack, type_of_element value)
 {
-    if (stack -> size >= (stack -> capacity) - 1)
+    stack_err_t code_error = stack_verify(stack, __FILE__, __func__, __LINE__);
+
+    if (code_error != NO_ERROR)
+        return code_error;
+
+    if (stack -> size == stack -> capacity) //>=
     {
         (stack -> capacity) *= MULTIPLIER;
         stack -> array = (type_of_element*)realloc(stack -> array, (stack -> capacity) * sizeof(type_of_element));
@@ -105,7 +151,7 @@ stack_err_t stack_push(stack_t *stack, type_of_element value)
     }
 
     stack -> size = (stack -> size) + 1;
-    stack -> array[stack -> size] = value;
+    stack -> array[(stack -> size) - 1] = value;
 
     return NO_ERROR;
 }
@@ -130,6 +176,8 @@ stack_err_t stack_creator(stack_t *stack, size_t initial_capacity)
 
 void stack_destructor(stack_t *stack)
 {
+    assert(stack != NULL);
+
     free(stack -> array);
 
     stack -> array = NULL;
@@ -138,75 +186,94 @@ void stack_destructor(stack_t *stack)
 }
 
 
-int stack_verify(stack_t *stack, const char *file_name, const char *function_name, int line)
+stack_err_t stack_verify(stack_t *stack, const char *file_name, const char *function_name, int line)
 {
-    int sum_of_errors = 0; // может лучше stack_err_t, но тогда проблемсы с приведением типов
-
+    stack_err_t type_of_error = NO_ERROR;
     if (stack == NULL)
     {
         printf("Stack has a null pointer\n");
-        sum_of_errors++;
+        type_of_error = ARRAY_POINTER_ERROR;
+
+        stack_dump(stack, type_of_error, file_name, function_name, line);
+        return type_of_error;
     }
 
     if ((stack -> array) == NULL)
     {
         printf("Array has a null pointer\n");
-        sum_of_errors++;
+        type_of_error = ARRAY_POINTER_ERROR;
+
+        stack_dump(stack, type_of_error, file_name, function_name, line);
+        return type_of_error;
     }
 
     if ((stack -> size) > STACK_MAX_SIZE)
     {
         printf("Size is bigger than MAX value\n");
-        sum_of_errors++;
+        type_of_error = OVER_FLOW;
+
+        stack_dump(stack, type_of_error, file_name, function_name, line);
+        return type_of_error;
     }
 
     if ((stack -> capacity) > STACK_MAX_CAPACITY)
     {
         printf("Capacity is bigger than MAX value\n");
-        sum_of_errors++;
+        type_of_error = OVER_FLOW;
+
+        stack_dump(stack, type_of_error, file_name, function_name, line);
+        return type_of_error;
     }
 
     if ((stack -> size) > (stack -> capacity))
     {
         printf("Size is bigger than capacity\n");
-        sum_of_errors++;
+        type_of_error = OVER_FLOW;
+
+        stack_dump(stack, type_of_error, file_name, function_name, line);
+        return type_of_error;
     }
 
     if ((stack -> size) < RESET_SIZE)
     {
         printf("Size is less than MIN value\n");
-        sum_of_errors++;
+        type_of_error = UNDER_FLOW;
+
+        stack_dump(stack, type_of_error, file_name, function_name, line);
+        return type_of_error;
     }
 
     if ((stack -> capacity) < RESET_CAPACITY)
     {
         printf("Capacity is less than MIN value\n");
-        sum_of_errors++;
+        type_of_error = UNDER_FLOW;
+
+        stack_dump(stack, type_of_error, file_name, function_name, line);
+        return type_of_error;
     }
 
-    if (sum_of_errors != 0)
-        stack_dump(stack, sum_of_errors, __FILE__, __func__, __LINE__);
-
-    return sum_of_errors;
+    return NO_ERROR;
 }
 
 
-void stack_dump(stack_t *stack, int sum_of_errors, const char *file_name, const char *function_name, int line)
+void stack_dump(stack_t *stack, stack_err_t type_of_error, const char *file_name, const char *function_name, int line)
 {
     size_t capacity_of_stack = stack -> capacity;
     size_t size_of_stack = stack -> size;
 
-    printf("There are %d errors int Stack_dump. It was called from function %s at %s line %d\n", sum_of_errors, function_name, file_name, line);
+    printf("There are %d errors int Stack_dump. It was called from function %s at %s line %d\n", type_of_error, function_name, file_name, line);
 
     printf("size = %lu\n", size_of_stack);
-    printf("size = %lu\n", capacity_of_stack);
-    printf("size = %p\n", stack -> array);
+    printf("capacity = %lu\n", capacity_of_stack);
+    printf("array[%p]\n", stack -> array);
 
     for (int index = 0; index < size_of_stack; index++)
         printf("*[%d] = %d\n", index, stack -> array[index]);
 
-    for (int index = 0; index >= (size_of_stack) && (index < capacity_of_stack); index++)
-        printf("[%d] = %d (POISON)\n", index, stack -> array[index]);
+    //for (int index = 0; index >= (size_of_stack) && (index < capacity_of_stack); index++)
+    //    printf("[%d] = %d (POISON)\n", index, stack -> array[index]);
+    for (int index = stack->size; index < stack->capacity; index++)
+        printf("  [%d] = %d (FREE)\n", index, stack->array[index]);
 }
 
 
